90821bb5192280aefb68dbfb6612f0900513db82
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-06-16 17:41:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//make nav bar hamburger menu options disappear on mobile and open to 5 menu items a fullsize
+
+
+
+//make social icons column block when fullsize
+
+
+//remove perLogo & secondaryNav margin:left on mobile
+
+
+
+//buttons red-orange glow slide
+//
+// const startSlide = ()=>{
+//     let swoosh = document.querySelector('.learner')
+//     swoosh.classList.toggle('slid')
+// }
+
+// let changeBkg = ()=>{
+//     let switched = document.querySelector('.videoPly')
+//     switched.setAttribute('src','https://i.gifer.com/S14x.gif')
+// }
